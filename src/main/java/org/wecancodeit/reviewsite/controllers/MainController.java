dd32e9fdbb25c291b8bf000bf01d2803b7d3ad64
Bini <abigiyabiniyam@gmail.com>
2019-02-19 15:29:57 -0500

    tried again

--- src/main/java/org/wecancodeit/reviewsite/controllers/MainController.java
+++ src/main/java/org/wecancodeit/reviewsite/controllers/MainController.java
@@ -1,84 +1,49 @@
 package org.wecancodeit.reviewsite.controllers;
 
-
 import javax.validation.Valid;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.wecancodeit.reviewsite.PersonForm;
 
 @Controller
-public class MainController implements WebMvcConfigurer {
+public class MainController {
 
-    @Override
+	@RequestMapping("/")
+	public String home() {
+		return "home";
+	}
+	
+	@RequestMapping("/individualPortfolio")
+	public String individualPortfolio() {
+		return "individualPortfolio";
+	}
+	
+	@RequestMapping("/results")
+	public String results() {
+		return "results";
+	}
+	
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/results").setViewName("results");
     }
 
-    @GetMapping("/")
+    @GetMapping("/form")
     public String showForm(PersonForm personForm) {
         return "form";
     }
 
-    @PostMapping("/")
+    @PostMapping("/results")
     public String checkPersonInfo(@Valid PersonForm personForm, BindingResult bindingResult) {
 
         if (bindingResult.hasErrors()) {
             return "form";
         }
 
-        return "redirect:/results";
+        return "results";
     }
 }
 
-
-//package org.wecancodeit.reviewsite.controllers;
-//
-//import org.springframework.stereotype.Controller;
-//import javax.validation.Valid;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.validation.BindingResult;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//import org.wecancodeit.reviewsite.UserForm;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//
-//@Controller
-//public class MainController {
-//
-//	@RequestMapping("/")
-//	public String home() {
-//		return "home";
-//	}
-//
-//	@RequestMapping("/individualPortfolio")
-//	public String individualPortfolio() {
-//		return "individualPortfolio";
-//	}
-//
-//	public void addViewControllers(ViewControllerRegistry registry) {
-//		registry.addViewController("/results").setViewName("results");
-//	}
-//
-//	@GetMapping("/form")
-//	public String showForm(UserForm userForm) {
-//		return "form";
-//	}
-//
-//	@PostMapping("/form")
-//	public String checkUserInfo(@Valid UserForm userForm, BindingResult bindingResult) {
-//
-//		if (bindingResult.hasErrors()) {
-//			return "form";
-//		}
-//
-//		return "redirect:/results";
-//	}
-//
-//}
