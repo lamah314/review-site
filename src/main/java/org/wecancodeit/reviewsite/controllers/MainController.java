cad24f122fb1d3a2ef3f3048726d6a3a0daf1e37
Anthony Lam <lamah314@gmail.com>
2019-02-19 14:14:44 -0500

    trying to add form code

--- src/main/java/org/wecancodeit/reviewsite/controllers/MainController.java
+++ src/main/java/org/wecancodeit/reviewsite/controllers/MainController.java
@@ -1,6 +1,14 @@
 package org.wecancodeit.reviewsite.controllers;
 
 import org.springframework.stereotype.Controller;
+import javax.validation.Valid;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.wecancodeit.reviewsite.UserForm;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,11 +19,35 @@ public class MainController {
 		return "home";
 	}
 	
-	@RequestMapping("/home-two")
-	public String homeTwo() {
-		return "homeTwo";
+	@RequestMapping("/individualPortfolio")
+	public String individualPortfolio() {
+		return "individualPortfolio";
 	}
 	
+	
+//	@RequestMapping("/form")
+//    public String form() {
+//        return "form";
+//    }
+	
+	public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/results").setViewName("results");
+    }
+	
+	@GetMapping("/form")
+    public String showForm(UserForm userForm) {
+        return "form";
+    }
+
+    @PostMapping("/form")
+    public String checkUserInfo(@Valid UserForm userForm, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            return "form";
+        }
+
+        return "redirect:/results";
+    }
 	
 }
 
