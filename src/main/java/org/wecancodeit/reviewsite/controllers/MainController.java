1f584df9b623cfa499c9478b751277bc620821ed
Nick Miner <nick.miner18@gmail.com>
2019-02-20 11:33:20 -0500

    basics!

--- src/main/java/org/wecancodeit/reviewsite/controllers/MainController.java
+++ src/main/java/org/wecancodeit/reviewsite/controllers/MainController.java
@@ -1,49 +1,48 @@
 package org.wecancodeit.reviewsite.controllers;
 
-import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.wecancodeit.reviewsite.PersonForm;
 
 @Controller
 public class MainController {
+	
+	List<PersonForm> people = new ArrayList<PersonForm>();
 
 	@RequestMapping("/")
 	public String home() {
 		return "home";
 	}
-	
+
 	@RequestMapping("/individualPortfolio")
 	public String individualPortfolio() {
 		return "individualPortfolio";
 	}
-	
+
 	@RequestMapping("/results")
-	public String results() {
+	public String results(Model model) {
+		// Figure out Model
 		return "results";
 	}
-	
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/results").setViewName("results");
-    }
 
-    @GetMapping("/form")
-    public String showForm(PersonForm personForm) {
-        return "form";
-    }
+//    public void addViewControllers() {
+//        registry.addViewController("/results").setViewName("results");
+//    }
 
-    @PostMapping("/results")
-    public String checkPersonInfo(@Valid PersonForm personForm, BindingResult bindingResult) {
+	@GetMapping("/form")
+	public String showForm() {
+		return "form";
+	}
 
-        if (bindingResult.hasErrors()) {
-            return "form";
-        }
-
-        return "results";
-    }
+	@PostMapping("/results")
+	public String checkPersonInfo(String username, String password) {
+		people.add(new PersonForm(username, password));
+		return "redirect:/results";
+	}
 }
-
