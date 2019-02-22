087ace5ef371ef513c2774737d8562297fab8c37
JimJewell <jimhjewell@gmail.com>
2019-02-22 10:51:51 -0500

    reviews-repo

---
+++ src/main/java/org/wecancodeit/reviewsite/repositories/ReviewsRepository.java
@@ -0,0 +1,38 @@
+package org.wecancodeit.reviewsite.repositories;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.wecancodeit.reviewsite.models.Review;
+
+public class ReviewsRepository {
+
+	private List<Review> reviews;
+	
+
+
+	public List<Review> getReviews() {
+		return reviews;
+	}
+
+	public void addReview(Review review) {
+		reviews.add(review);
+	}
+
+	@Override
+	public String toString() {
+		return "ReviewsRepository [reviews=" + reviews + "]";
+	}
+
+	public List<Review> listPortfolioReviews(String portfolioName) {
+		List<Review> reviewList = new ArrayList<Review>();
+		for (Review review: reviews) {
+			if(review.getPortfolioName().equals(portfolioName)) {
+				reviewList.add(review);
+			}
+		}
+		return reviewList;
+	}
+	
+	
+}
